e388f600a9dd68f2048d8e9e29bdfc3cdc4f21f6
Aleksei Maslakov <lesha.maslakov@gmail.com>
2017-12-07 23:41:10 +0300

    add pytest

--- backend/phonebook/companies/models.py
+++ backend/phonebook/companies/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse
 
 
-# Create your models here.
 class CompanyManager(models.Manager):
     def get_queryset(self):
         queryset = super(CompanyManager, self).get_queryset()
