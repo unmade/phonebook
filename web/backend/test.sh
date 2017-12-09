95b6509b2afda6510063297cb2420914726bd88b
Aleksei Maslakov <lesha.maslakov@gmail.com>
2017-12-09 12:21:27 +0300

    embedded travis ci build status

--- web/backend/test.sh
+++ web/backend/test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+cd .. && isort --check-only -q && pylint phonebook/* --errors-only && cd -
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 cd .. && make test
