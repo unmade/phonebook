57c5c230ded45de5f977acd79671bfb0e192bbb6
Aleksei Maslakov <lesha.maslakov@gmail.com>
2017-12-22 19:34:07 +0300

    add docker-compose for local development

--- backend/phonebook/phonebook/settings/local.py
+++ backend/phonebook/phonebook/settings/local.py
@@ -17,3 +17,12 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 INTERNAL_IPS = ('127.0.0.1',)
 
 ROOT_URLCONF = '%s.urls.local' % SITE_NAME
+
+
+def show_toolbar(request):
+    return DEBUG
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': 'phonebook.settings.local.show_toolbar',
+}
