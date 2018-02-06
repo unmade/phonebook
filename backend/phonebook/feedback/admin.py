82c7366d80c237bc8c0e30062745b5e8c841ee3b
Aleksei Maslakov <lesha.maslakov@gmail.com>
2018-02-06 21:20:25 +0300

    test exit code in travis

--- backend/phonebook/feedback/admin.py
+++ backend/phonebook/feedback/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from .models import Feedback
 
-
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('sender', 'text', 'created_at', 'status')
